ba63fe0adeae9f5676248f77113a61d1d7ba70d3
Syed Sarfaraz Akhtar <syedsarfarazakhtar@gmail.com>
2019-10-01 10:45:40 -0400

    fixed learning rate

--- src/run.py
+++ src/run.py
@@ -8,9 +8,8 @@ import numpy as np
 def predict(input):
 
     out, hidden = mymodel(input)
-    char_ind = torch.max(out, dim=1)
-    #char_ind = torch.max(prob, dim=0)[1].item()
-    return(char_ind)
+    return torch.max(out, dim=1)
+    #return out.max(1, keepdim=True)
 
 # torch.cuda.is_available() checks and returns a Boolean True if a GPU is available, else it'll return False
 is_cuda = torch.cuda.is_available()
@@ -24,45 +23,90 @@ else:
     print("GPU not available, CPU used")
 
 samples, cols = get_features()
-mymodel = model.Model(input_size=len(cols), output_size=2, hidden_dim=50, n_layers=2)
+mymodel = model.Model(input_size=len(cols), output_size=2, hidden_dim=100, n_layers=2)
 
-n_epochs = 40
-lr = 0.0001
+n_epochs = 500
+lr = 0.00001
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(mymodel.parameters(), lr=lr)
 mymodel.to(device)
 
+training_set = []
+evaluation_set = []
+testing_set = []
+
+for sample in samples:
+    for i in sample:
+        x = i
+        X_train = x[0]
+        X_train = np.asanyarray(X_train)
+        y_train_str = np.asarray(x[1])
+        if len(X_train) == 0:
+            continue
+        is_cuda = torch.cuda.is_available()
+
+        if is_cuda:
+            device = torch.device("cuda")
+        else:
+            device = torch.device("cpu")
+
+        X_train = torch.from_numpy(X_train).float()
+        X_train = X_train.unsqueeze(0)
+        y_train = []
+        for i in y_train_str:
+            if i == "accented":
+                y_train.append([1, 0])
+            else:
+                y_train.append([0, 1])
+
+        y_train = torch.FloatTensor(y_train).float()
+        y_train = y_train.unsqueeze(0)
+        X_train.to(device)
+        y_train.to(device)
+        training_set.append([X_train, y_train])
+
+for sample in samples:
+    for i in sample:
+        x = i
+        X_test = x[2]
+        X_test = np.asanyarray(X_test)
+        y_test_str = np.asarray(x[3])
+        if len(X_test) == 0:
+            continue
+        is_cuda = torch.cuda.is_available()
+
+        if is_cuda:
+            device = torch.device("cuda")
+        else:
+            device = torch.device("cpu")
+
+        X_test = torch.from_numpy(X_test).float()
+        X_test = X_test.unsqueeze(0)
+        y_test = []
+        for i in y_test_str:
+            if i == "accented":
+                y_test.append([1, 0])
+            else:
+                y_test.append([0, 1])
+
+        y_test = torch.FloatTensor(y_test).float()
+        y_test = y_test.unsqueeze(0)
+        X_test.to(device)
+        y_test.to(device)
+        testing_set.append([X_test, y_test])
+
+validation_set = testing_set[0:int(len(testing_set)/2)]
+testing_set = testing_set[int(len(testing_set)/2)::]
+print (len(training_set), len(validation_set), len(testing_set))
+
 for epoch in range(1, n_epochs + 1):
 
-    for sample in samples:
-        for i in sample:
+    if True:
+        for i in training_set:
             x = i
             X_train = x[0]
-            X_train = np.asanyarray(X_train)
-            y_train_str = np.asarray(x[1])
-            if len(X_train) == 0:
-                continue
-            is_cuda = torch.cuda.is_available()
-
-            if is_cuda:
-                device = torch.device("cuda")
-            else:
-                device = torch.device("cpu")
-
-            X_train = torch.from_numpy(X_train).float()
-            X_train = X_train.unsqueeze(0)
-            y_train = []
-            for i in y_train_str:
-                if i == "accented":
-                    y_train.append([1, 0])
-                else:
-                    y_train.append([0, 1])
-
-            y_train = torch.FloatTensor(y_train).float()
-            y_train = y_train.unsqueeze(0)
-            X_train.to(device)
-            y_train.to(device)
+            y_train = x[1]
 
             cross_entropy_target = []
             for i in y_train[0]:
@@ -80,18 +124,54 @@ for epoch in range(1, n_epochs + 1):
     if epoch % 1 == 0:
         print('Epoch: {}/{}.............'.format(epoch, n_epochs), end=' ')
         print("Loss: {:.4f}".format(loss.item()))
+        total_samples = 0.0
+        total_correct = 0.0
+        true_pos = 0.0
+        false_neg = 0.0
+        true_neg = 0.0
+        false_pos = 0.0
 
+        if True:
+            for i in validation_set:
+                x = i
+                X_val = x[0]
+                Y_val = x[1]
+                O_val = predict(X_val)
+                correct_out = Y_val
 
-for sample in samples:
-    for i in sample:
+                predictions = []
+                for i in O_val:
+                    predictions.append(i)
+                O_val = predictions[1]
+                le = O_val.size(0)
+                for k in range(le):
+                    if int(O_val[k].item()) == int(correct_out[0][k][0].item()):
+                        total_correct += 1
+                        if int(correct_out[0][k][0].item()) == 1:
+                            true_pos += 1
+                        if int(correct_out[0][k][0].item()) == 0:
+                            true_neg += 1
+                    else:
+                        if int(correct_out[0][k][0].item()) == 1:
+                            false_neg += 1
+                        if int(correct_out[0][k][0].item()) == 0:
+                            false_pos += 1
+                    total_samples += 1
+        precision = true_pos*1.0/(true_pos+false_pos)
+        recall = true_pos*1.0/(true_pos+false_neg)
+        f1 = 2.0*(recall*precision)/(recall+precision)
+        accuracy = total_correct*1.0/total_samples
+        print("Total Samples: ", total_samples)
+        print("Accuracy: ", accuracy)
+        print("Precision: ", precision)
+        print("Recall: ", recall)
+        print("F1: ", f1)
+        print()
+
+if False:
+    for i in testing_set:
         x = i
-        X_test = x[2]
-        if len(X_test) == 0:
-            continue
-        X_test = torch.from_numpy(X_test).float()
-        X_test = X_test.unsqueeze(0)
-        X_test.to(device)
-
-        print (predict(X_test))
+        X_test = x[0]
+        print(predict(X_test))
 
 
